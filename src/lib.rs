2da5eb09863f491aa7728c90614bf050613e8089
Dominick Allen <dominick.allen1989@gmail.com>
2015-04-10 09:42:58 -0400

    working on calling code from C

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![crate_name = "XBTVEd"]
 #![crate_type = "dylib"]
-#![feature(convert, collections, unboxed_closures)]
+#![feature(libc, convert, collections, unboxed_closures)]
+
+extern crate libc;
+
+use std::ffi::CString;
+use std::mem;
 
 pub mod action;
 pub mod parse;
@@ -12,6 +17,28 @@ pub mod gui;
 pub use gui::{XBTVEd, EdBuffer};
 
 #[no_mangle]
-pub extern fn times2(num: isize) -> isize {
-    num * 2
+pub extern fn create_app() -> *const XBTVEd {
+    let app = Box::new(XBTVEd::new());
+    unsafe {
+        mem::transmute(app)
+    }
+}
+
+#[no_mangle]
+pub extern fn display_name(xbtved: *const XBTVEd) -> *const i8 {
+    unsafe {
+        match CString::new((*xbtved).current_buffer().get_name()) {
+            Ok(x) => x.as_ptr(),
+            Err(_) => CString::new("SCREWED UP!").unwrap().as_ptr()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern fn buffers_len(xbtved: *const XBTVEd) -> u32 {
+    unsafe {
+        let res = (*xbtved).buffers_len();
+        println!("{}", res);
+        res as u32
+    }
 }
